9cc7204da28eb9884e454bc3bb4a5252f375be9d
Remy Joseph <axel080802@gmail.com>
2022-10-05 21:32:54 +0200

    chef et villages

---
+++ src/personnages/Village.java
@@ -0,0 +1,30 @@
+package personnages;
+
+public class Village {
+	private String nom;
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois=0;
+	
+	public Village(String nom,int nbVillageoisMaximum) {
+	this.nom = nom;
+	this.villageois=new Gaulois[nbVillageoisMaximum];
+	}
+	public void ajouterHabitant(Gaulois gaulois) {
+		if(nbVillageois<villageois.length) {
+			villageois[nbVillageois]=gaulois;
+			nbVillageois++;
+		}
+	}
+	public Gaulois trouverHabitant(int numVillageois) {
+		
+			return villageois[numVillageois];
+	}
+	public void setChef(Chef chef) {
+	this.chef = chef;
+	}
+	public String getNom() {
+	return nom;
+	}
+
+}
